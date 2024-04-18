21b7bd34dcab06bd6d05085b1de57c7436dae033
wooyong99 <wulovesk@naver.com>
2024-04-18 15:46:25 +0900

    cherrypick KSP-88

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import HomePage from './pages/Home';
+import Main from './pages/Home/Main';
 import ProfilePage from './pages/Profile';
+import ReviewRegister from './pages/Review/register';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: <Main />,
   },
   {
     path: '/profile',
     element: <ProfilePage />,
+  },
+  {
+    path: '/review-register',
+    element: <ReviewRegister />,
   },
 ]);
