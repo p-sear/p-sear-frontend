331072045907d54750d65c91d4ec5890ffe5c09e
ChoiHyebin <bini0819@catholic.ac.kr>
2024-04-20 11:41:35 +0900

    [KSP-115] style: Header user-icon 이미지에서 react-icons로 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../../assets/images/logo.png';
-import userIcon from '../../assets/icons/user-icon.png';
+import { FaRegUserCircle } from "react-icons/fa";
 import './Header.css';
 
 const Header = () => {
@@ -9,10 +9,10 @@ const Header = () => {
             <header className='flex content-center justify-between'>
                 <a href="/"><img src={logo} alt="" className='logo'/></a>
 
-                <div className='flex content-center justify-center cursor-pointer header-name'>
+                <button className='flex items-center justify-center cursor-pointer header-name'>
                     <span>최혜빈님</span>
-                    <img src={userIcon} alt="" className='user-icon'/>
-                </div>
+                    <FaRegUserCircle style={{fontSize: '20px'}}/>
+                </button>
             </header>
         </>
     );
