86fbe3fc7700a7a50c3bffdb034f2707c1edf976
ChoiHyebin <bini0819@catholic.ac.kr>
2024-04-24 09:21:15 +0900

    [KSP-138] feat: 로그인 컴포넌트 구현

--- src/pages/Account/Login.jsx
+++ src/pages/Account/Login.jsx
@@ -4,8 +4,23 @@ import SocialLogin from './SocialLogin';
 
 const Login = () => {
     return (
-        <div>
-            login
+        <div className="login-container flex flex-col">
+            <div className='login-form flex flex-col'>
+                <div className="email-box flex flex-col">
+                    <h1>이메일</h1>
+                    <input type="email" placeholder='이메일을 입력해주세요'/>
+                </div>
+                <div className="pw-box flex flex-col">
+                    <h1>비밀번호</h1>
+                    <input type="password" placeholder='비밀번호를 입력해주세요' />
+                </div>
+            </div>
+
+            <button className='login-btn'>로그인</button>
+
+            <div>
+                <SocialLogin />
+            </div>
         </div>
     )
 }
