b51c601d6f48f3c422769a1fc1589c2b7c4feaa3
ChoiHyebin <bini0819@catholic.ac.kr>
2024-04-30 15:08:06 +0900

    refactor: StepProgress에 접근시 접근 안 되는 문제 해결

--- src/pages/HotelRegistration/StepProgress.jsx
+++ src/pages/HotelRegistration/StepProgress.jsx
@@ -15,17 +15,22 @@ const StepProgress = () => {
 
     const navigate = useNavigate();
 
-    // currentProgress 상태가 변경될 때마다 localStorage에 저장
     useEffect(() => {
         localStorage.setItem('currentProgress', currentProgress);
-        // 마지막 단계(7단계)에서 '홈 화면으로 이동' 버튼 클릭 시 홈 화면으로 이동
-        if (currentProgress === 7) {
-            navigate('/');
+        if (currentProgress == 6) {
+            // '홈 화면으로 이동' 버튼으로 메인 화면으로 이동 시, 로컬 스토리지 초기화
+            // 초기화 로직은 여기가 아니라, 이동 버튼 클릭 핸들러에 추가 -> 여기에 추가하게 되면 '다음' 버튼을 누를 때마다 currentProgress + 1이 되는데 5에서 6으로 넘어가는 순간 바로 로컬 스토리지가 초기화되고 메인 화면으로 이동하게 됨
         }
     }, [currentProgress, navigate]);
 
     const updateProgress = (newProgress) => {
         setCurrentProgress(newProgress);
+    };
+
+    const goToHome = () => {
+        // 여기에 로컬 스토리지를 초기화하는 로직 추가
+        localStorage.removeItem('currentProgress'); // 혹은 localStorage.setItem('currentProgress', 1);
+        navigate('/');
     };
 
     const stepComponents = {
@@ -46,11 +51,16 @@ const StepProgress = () => {
             </div>
 
             {stepComponents[currentProgress]}
-            
+
             <button
                 id="next" 
-                disabled={currentProgress === progressArr.length} 
-                onClick={() => updateProgress(currentProgress + 1)}
+                onClick={() => {
+                    if (currentProgress === 6) {
+                        goToHome();  // 6단계에서 버튼 클릭 시 홈 화면으로 이동
+                    } else {
+                        updateProgress(currentProgress + 1);  // 그 외 단계에서는 다음 단계로 진행
+                    }
+                }}
                 className='next-btn'
             >
                 {currentProgress === 1 ? '시작하기' : currentProgress === 5 ? '등록 완료' : currentProgress === 6 ? '홈 화면으로 이동' : '다음'}
