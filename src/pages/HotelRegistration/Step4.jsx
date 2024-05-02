00636a368eca918d490a7c23b5edf71407d3fd7f
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-02 10:06:59 +0900

    style: step4 컴포넌트 글씨 굵기 및 placeholder 설정

--- src/pages/HotelRegistration/Step4.jsx
+++ src/pages/HotelRegistration/Step4.jsx
@@ -9,18 +9,18 @@ const Step4 = () => {
             <div className='step4-box flex flex-col justify-center items-center'>
                 <div className="step4-name flex flex-col justify-center items-center gap-3">
                     <h3>이름</h3>
-                    <input type="text" />
+                    <input type="text" placeholder='이름을 입력해주세요'/>
                 </div>
                 <div className="step4-tel flex flex-col justify-center items-center gap-3">
                     <h3>연락처</h3>
-                    <input type="tel" />
+                    <input type="tel" placeholder='휴대폰 번호를 입력해주세요'/>
                 </div>
                 <div className="step4-email flex flex-col justify-center items-center gap-3">
                     <div className='w-full relative text-center'>
                         <h3>이메일</h3>
                         <button className='absolute right-0 top-0'>인증</button>
                     </div>
-                    <input type="email" />
+                    <input type="email" placeholder='이메일을 입력해주세요'/>
                 </div>
             </div>
         </div>
