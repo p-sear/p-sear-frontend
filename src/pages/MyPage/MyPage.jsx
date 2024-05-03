8c50e3cb828cb4ab08fb21667e5e92b8d731bc2a
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-03 16:32:28 +0900

    refactor: MyPage 컴포넌트 구현

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -4,15 +4,28 @@ import './MyPage.css';
 import MyInfo from './MyInfo';
 import MyReservation from './MyReservation';
 import MySetting from './MySetting';
+import { IoIosArrowDropright } from "react-icons/io";
 
 const MyPage = () => {
     return (
-        <div className='mypage-container'>
-            <nav>
-                <NavLink to="/profile/info" style={({ isActive }) => ({ color: isActive ? 'blue' : 'black' })}>내 정보 확인</NavLink>
-                <NavLink to="/profile/reservations" style={({ isActive }) => ({ color: isActive ? 'blue' : 'black' })}>예약 내역</NavLink>
-                <a href="/hotel/new">숙소 등록</a>
-                <NavLink to="/profile/settings" style={({ isActive }) => ({ color: isActive ? 'blue' : 'black' })}>설정</NavLink>
+        <div className='mypage-container flex'>
+            <nav className='flex flex-col justify-center'>
+                <NavLink to="/profile/info" className={({ isActive }) => (isActive ? 'link link-active' : 'link')}>
+                    나의 정보
+                    <IoIosArrowDropright className='right-arrow-icon'/>
+                </NavLink>
+                <NavLink to="/profile/reservations" className={({ isActive }) => (isActive ? 'link link-active' : 'link')}>
+                    예약 내역
+                    <IoIosArrowDropright className='right-arrow-icon' />
+                </NavLink>
+                <a href="/hotel/new" className='flex items-center justify-between' style={{color: 'gray'}}>
+                    숙소 등록
+                    <IoIosArrowDropright className='right-arrow-icon' />
+                </a>
+                <NavLink to="/profile/settings" className={({ isActive }) => (isActive ? 'link link-active' : 'link')}>
+                    설정
+                    <IoIosArrowDropright className='right-arrow-icon' />
+                </NavLink>
             </nav>
             <Routes>
                 <Route path="/" element={<Navigate replace to="/profile/info" />} />
