9b7ec434bfbd76b2a4b2a9bc529093bacaf6028e
LEE <dlcksgur0919@naver.com>
2024-05-07 11:02:41 +0900

    ✨

--- src/pages/HotelAution/auction.jsx
+++ src/pages/HotelAution/auction.jsx
@@ -9,6 +9,9 @@ import { Typography } from '@material-tailwind/react';
 import { Input } from '@material-tailwind/react';
 import { Button } from '@material-tailwind/react';
 
+import MyApp from '../HotelInquiry/MyApp';
+import AuctionBar from './auctionBar';
+
 const data = [
   {
     date: '24/04/30/12:00',
@@ -28,43 +31,46 @@ const Auction = () => {
   const [open, setOpen] = React.useState(1);
   const handleOpen = value => setOpen(open === value ? 0 : value);
   return (
-    <div className='flex justify-center'>
-      <Accordion
-        open={open === 0}
-        className=' m-4 w-2/3 rounded-lg border border-blue-gray-100 bg-blue-50 px-4'
-      >
-        <AccordionHeader className='flex justify-between'>
-          <Typography variant='h5' className='ml-8 mr-8'>
-            남은시간 : 72 : 00 : 00
-          </Typography>
-
-          <Typography variant='h5' className='mr-4'>
-            24/04/30/13:00 80,000원
-          </Typography>
-
-          <div className='mr-8 w-2/12'>
-            <Input label='금액 입력'></Input>
-          </div>
-
-          <Button color='blue'>입찰</Button>
-          <div className='justify-end'>
-            <Button color='blue' onClick={() => handleOpen(1)}>
-              더보기
-            </Button>
-          </div>
-        </AccordionHeader>
-
-        {data.map(data => (
-          <AccordionBody className=''>
-            <Typography variant='h5' className='float-left'>
-              {data.date}
+    <div>
+      <div className='flex justify-center'>
+        <Accordion
+          open={open === 0}
+          className=' m-4 w-2/3 rounded-lg border border-blue-gray-100 bg-blue-50 px-4'
+        >
+          <AccordionHeader className='flex justify-between'>
+            <Typography variant='h5' className='ml-8 mr-8'>
+              남은시간 : 72 : 00 : 00
             </Typography>
-            <Typography variant='h5' className='text-right'>
-              {data.price}
+
+            <Typography variant='h5' className='mr-4'>
+              24/04/30/13:00 80,000원
             </Typography>
-          </AccordionBody>
-        ))}
-      </Accordion>
+
+            <div className='mr-8 w-2/12'>
+              <Input label='금액 입력'></Input>
+            </div>
+
+            <Button color='blue'>입찰</Button>
+            <div className='justify-end'>
+              <Button color='blue' onClick={() => handleOpen(1)}>
+                더보기
+              </Button>
+            </div>
+          </AccordionHeader>
+
+          {data.map(data => (
+            <AccordionBody className=''>
+              <Typography variant='h5' className='float-left'>
+                {data.date}
+              </Typography>
+              <Typography variant='h5' className='text-right'>
+                {data.price}
+              </Typography>
+            </AccordionBody>
+          ))}
+        </Accordion>
+      </div>
+      <AuctionBar></AuctionBar>
     </div>
   );
 };
---
+++ src/pages/HotelAution/auctionBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { Typography } from '@material-tailwind/react';
+import { Button } from '@material-tailwind/react';
+import { Card } from '@material-tailwind/react';
+import { Input } from '@material-tailwind/react';
+
+const AuctionBar = () => {
+  return (
+    <div className='m-4 flex justify-end'>
+      <Card className=' jus flex w-1/6 bg-blue-50 '>
+        <div className='m-3 flex flex-col'>
+          <Typography variant='h4' color='blue' className='m-1 text-center'>
+            입찰하기
+          </Typography>
+
+          <Typography variant='h6'>남은시간</Typography>
+          <Typography variant='h6'>시작가격</Typography>
+          <Typography variant='h6' className='text-center'>
+            현재 최고 입찰가
+          </Typography>
+          <Typography variant='h5' color='red' className='text-center'>
+            60,000원
+          </Typography>
+          <Typography variant='h6' className='text-center'>
+            즉시 입찰가
+          </Typography>
+          <Typography variant='h5' color='red' className='text-center'>
+            75,000원
+          </Typography>
+          <Typography variant='h6' className='text-center'>
+            입찰가
+          </Typography>
+
+          <div className=' m-2 flex justify-center'>
+            <Input label='금액 입력'></Input>
+          </div>
+
+          <div className=' m-2 flex justify-center'>
+            <Button className='m-1' color='blue'>
+              입찰하기
+            </Button>
+            <Button className='m-1' color='red'>
+              즉시입찰
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default AuctionBar;
