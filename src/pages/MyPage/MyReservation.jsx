318038a820a1fc52ba52c35259ceffd7df6448e1
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-07 15:38:31 +0900

    refactor: MyReservation 구조 수정

--- src/pages/MyPage/MyReservation.jsx
+++ src/pages/MyPage/MyReservation.jsx
@@ -40,32 +40,28 @@ const MyReservation = () => {
             <div className="myreservation-box w-full flex flex-col justify-center items-center">
 
                 {reservations.map((reservation) => (
-                <div className='myreservation-item w-full flex justify-between items-center gap-20 h-full'>
+                <div className='myreservation-item w-full flex justify-between items-center gap-20'>
                     <div className='flex items-center h-full'>
                         <img src={hotelImg} alt="" />
-                        <div className='flex flex-col justify-between h-full'>
-                            <div className='flex justify-between'>
+
+                        <div className='flex items-center h-full'>
+                            <div className='flex flex-col justify-between h-full'>
                                 <p>숙소</p>
-                                <p>{reservation.name}</p>
-                            </div>
-                            <div className='flex justify-between'>
                                 <p>객실</p>
-                                <p>{reservation.roomType}</p>
-                            </div>
-                            <div className='flex justify-between'>
                                 <p>체크인/체크아웃</p>
-                                <p>{reservation.checkIn} ~ {reservation.checkOut}</p>
-                            </div>
-                            <div className='flex justify-between'>
                                 <p>결제 금액</p>
+                            </div>
+                            <div className='flex flex-col justify-between font-bold h-full'>
+                                <p>{reservation.name}</p>
+                                <p>{reservation.roomType}</p>
+                                <p>{reservation.checkIn} ~ {reservation.checkOut}</p>
                                 <p>{reservation.price} 원</p>
                             </div>
                         </div>
+
                     </div>
 
-                    <div className='h-full flex flex-col'>
-                        <button className='detail-btn'>상세 조회 &gt;</button>
-                    </div>
+                    <button className='detail-btn'>상세 조회 &gt;</button>
                 </div>
                 ))}
 
