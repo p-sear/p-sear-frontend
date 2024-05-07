4ecf8e8e2c9c9ef1662b98d59958529e1b48e5a9
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-07 16:53:30 +0900

    refactor: MySetting 체크박스 로직 구현

--- src/pages/MyPage/MySetting.jsx
+++ src/pages/MyPage/MySetting.jsx
@@ -1,64 +1,51 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './MySetting.css';
-import { FaPlus, FaMinus } from "react-icons/fa6";
 
 const MySetting = () => {
-    const[isOpen, setIsOpen] = useState(false);
-    const toggleMenu = () => {
-        setIsOpen(isOpen=>!isOpen);
-    }
+    const [isEmailChecked, setIsEmailChecked] = useState(false);
+    const [isSmsChecked, setIsSmsChecked] = useState(false);
+    const [isKakaoChecked, setIsKakaoChecked] = useState(false);
 
-    const [allChecked, setAllChecked] = useState(false);
-    const [checkboxes, setCheckboxes] = useState({
-        checkbox1: false,
-        checkbox2: false,
-        checkbox3: false,
-    });
+    const isAllChecked = isEmailChecked || isSmsChecked || isKakaoChecked;
 
-    const handleAllChecked = (event) => {
-        const isChecked = event.target.checked;
-        setAllChecked(isChecked);
-        setCheckboxes({
-        checkbox1: isChecked,
-        checkbox2: isChecked,
-        checkbox3: isChecked,
-        });
+    const handleCheckboxChange = (setter) => (e) => {
+        setter(e.target.checked);
     };
 
-    const handleSingleCheck = (event) => {
-        const { name, checked } = event.target;
-        setCheckboxes({
-        ...checkboxes,
-        [name]: checked,
-        });
+    const handleAllCheckedChange = (e) => {
+        const isChecked = e.target.checked;
+        setIsEmailChecked(isChecked);
+        setIsSmsChecked(isChecked);
+        setIsKakaoChecked(isChecked);
     };
 
     return (
         <div className="mysetting-container flex flex-col">
-            <h1>설정</h1>
+            <div>
+                <h1>설정</h1>
+                <p>경매 마감, 타임 특가 등 이벤트 정보를 빠르게 알려드립니다.</p>
+            </div>
 
             <div className="mysetting-box">
-                <div className='flex justify-between items-center'>
-                    <p className='flex justify-center items-center gap-3'>
-                        <input type="checkbox" style={{scale: '1.5'}} checked={allChecked} onChange={handleAllChecked}/>
+                <div>
+                    <p className='flex items-center gap-3'>
+                        <input type="checkbox" style={{scale: '1.5'}} checked={isAllChecked} onChange={handleAllCheckedChange}/>
                         <span>알림 수신 동의(선택)</span>
                     </p>
-                </div>
-                <div className='mysetting-detail-terms'>
                     <p className='flex items-center gap-3'>
                         <p>⌞</p>
-                        <input type="checkbox" style={{scale: '1.5'}} name="checkbox1" checked={checkboxes.checkbox1} onChange={handleSingleCheck}/>
+                        <input type="checkbox" style={{scale: '1.5'}} checked={isEmailChecked} onChange={handleCheckboxChange(setIsEmailChecked)}/>
                         <span>이메일</span>
                     </p>
                     <p className='flex items-center gap-3'>
                         <p>⌞</p>
-                        <input type="checkbox" style={{scale: '1.5'}} name="checkbox2" checked={checkboxes.checkbox2} onChange={handleSingleCheck}/>
+                        <input type="checkbox" style={{scale: '1.5'}}  checked={isSmsChecked} onChange={handleCheckboxChange(setIsSmsChecked)}/>
                         <span>문자</span>
                     </p>
                     <p className='flex items-center gap-3'>
                         <p>⌞</p>
-                        <input type="checkbox" style={{scale: '1.5'}} name="checkbox3" checked={checkboxes.checkbox3} onChange={handleSingleCheck}/>
+                        <input type="checkbox" style={{scale: '1.5'}} checked={isKakaoChecked} onChange={handleCheckboxChange(setIsKakaoChecked)}/>
                         <span>카카오톡</span>
                     </p>
                 </div>
