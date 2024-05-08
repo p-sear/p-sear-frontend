f0579fd86b3acd485e12f4f59febdeea13e41132
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-08 14:53:56 +0900

    style: MyBookmark 컴포넌트 css 적용

--- src/pages/MyPage/MyBookmark.jsx
+++ src/pages/MyPage/MyBookmark.jsx
@@ -8,33 +8,24 @@ const MyBookmark = () => {
             id: 1,
             photo: hotelImg,
             name: '호텔 A',
-            roomType: '스탠다드',
-            checkIn: '2024.05.20',
-            checkOut: '2024.05.22',
-            price: '100,000',
+            price: '150,000',
         },
         {
             id: 2,
             photo: hotelImg,
             name: '호텔 B',
-            roomType: '스위트',
-            checkIn: '2024.05.20',
-            checkOut: '2024.05.22',
-            price: '100,000',
+            price: '90,000',
         },
         {
             id: 3,
             photo: hotelImg,
             name: '호텔 C',
-            roomType: '스탠다드',
-            checkIn: '2024.05.20',
-            checkOut: '2024.05.22',
-            price: '100,000',
+            price: '88,000',
         }
     ]
 
     return (
-        <div className="mybookmark-container">
+        <div className="mybookmark-container flex flex-col">
             <h1>찜한 숙소</h1>
 
             <div className="mybookmark-box w-full flex flex-col justify-center items-center">
@@ -45,23 +36,19 @@ const MyBookmark = () => {
                         <img src={hotelImg} alt="" />
 
                         <div className='flex items-center h-full'>
-                            <div className='flex flex-col justify-between h-full'>
+                            <div className='flex flex-col justify-evenly h-full'>
                                 <p>숙소</p>
-                                <p>객실</p>
-                                <p>체크인/체크아웃</p>
-                                <p>결제 금액</p>
+                                <p>숙박 가격</p>
                             </div>
-                            <div className='flex flex-col justify-between font-bold h-full'>
+                            <div className='flex flex-col justify-evenly font-bold h-full'>
                                 <p>{reservation.name}</p>
-                                <p>{reservation.roomType}</p>
-                                <p>{reservation.checkIn} ~ {reservation.checkOut}</p>
                                 <p>{reservation.price} 원</p>
                             </div>
                         </div>
 
                     </div>
 
-                    <button className='mybook-detail-btn'>상세 조회 &gt;</button>
+                    <button className='mybook-reserve-btn'>예약하기 &gt;</button>
                 </div>
                 ))}
 
