b7562a97938c915725c48c4ad471df8e92607253
LEE <dlcksgur0919@naver.com>
2024-05-08 09:23:32 +0900

    ✨

--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import AccountForm from './pages/Account/AccountForm';
 import Main from './pages/Home/Main';
-
 import Auction from './pages/HotelAution/auction';
+import HotelDetail from './pages/HotelDetails/HotelDetail';
 import MyApp from './pages/HotelInquiry/MyApp';
+import StepProgress from './pages/HotelRegistration/StepProgress';
+import HotelReservation from './pages/HotelReservation/hotelReservation';
+import HotelReservation2 from './pages/HotelReservation/hotelReservation2';
 import ReviewRegister from './pages/Review/register';
 
 export const router = createBrowserRouter([
@@ -16,14 +19,34 @@ export const router = createBrowserRouter([
     path: '/login',
     element: <AccountForm />,
   },
+
   {
-    path: '/profile',
-    element: <ProfilePage />,
+    path: '/step',
+    element: <StepProgress />,
   },
-  
+
   {
     path: '/myapp',
     element: <MyApp />,
+  },
+  {
+    path: '/hotel-auction',
+    element: <Auction />,
+  },
+  {
+    path: '/hotel-detail',
+    element: <HotelDetail />,
+  },
+
+  {
+    path: '/hotel-reservation',
+    element: <HotelReservation />,
+  },
+  {
+    path: '/hotel-reservation2',
+    element: <HotelReservation2 />,
+  },
+
   {
     path: '/review-register',
     element: <ReviewRegister />,
