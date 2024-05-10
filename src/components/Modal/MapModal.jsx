8c97ab872664a953150ff00e6e943391290a3784
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-10 09:04:36 +0900

    feat: MapModal 컴포넌트 생성

---
+++ src/components/Modal/MapModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MapModal = () => {
+    return (
+        <div className="mapmodal-container">
+            map modal
+        </div>
+    )
+}
+
+export default MapModal;
