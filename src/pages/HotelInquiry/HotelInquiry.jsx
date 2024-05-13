0585fb24818e8d07b98a071c8eb4f1ed01ae039e
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-13 17:29:28 +0900

    fix: 사용하지 않는 import 제거

--- src/pages/HotelInquiry/HotelInquiry.jsx
+++ src/pages/HotelInquiry/HotelInquiry.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 import Calendar from './Calendar';
 import './HotelInquiry.css';
 
 const HotelInquiry = () => {
   return (
     <>
-      <div class='bg-primary-darken text-bold inline-flex items-center rounded bg-green-300 p-4 text-white shadow-md '>
+      <div className='bg-primary-darken text-bold inline-flex items-center rounded bg-green-300 p-4 text-white shadow-md '>
         Not Pretty Code 😵
       </div>
       <Calendar></Calendar>
