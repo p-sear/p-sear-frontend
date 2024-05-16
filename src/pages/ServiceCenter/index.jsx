b6698b4857f4446b13c6e229b6e5580a01f0b19b
wooyong99 <wulovesk@naver.com>
2024-05-16 14:57:32 +0900

    fix : props type 검사 오류

--- src/pages/ServiceCenter/index.jsx
+++ src/pages/ServiceCenter/index.jsx
@@ -6,29 +6,28 @@ import {
   AccordionHeader,
 } from '@material-tailwind/react';
 
-function Icon({ id, open }) {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
-      strokeWidth={2}
-      stroke='currentColor'
-      className={`${id === open ? 'rotate-180' : ''} h-5 w-5 transition-transform`}
-    >
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M19.5 8.25l-7.5 7.5-7.5-7.5'
-      />
-    </svg>
-  );
-}
-
 const ServiceCenter = () => {
   const [open, setOpen] = React.useState(0);
-
   const handleOpen = value => setOpen(open === value ? 0 : value);
+  // eslint-disable-next-line react/prop-types
+  const Icon = ({ id, open }) => {
+    return (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={2}
+        stroke='currentColor'
+        className={`${id === open ? 'rotate-180' : ''} h-5 w-5 transition-transform`}
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M19.5 8.25l-7.5 7.5-7.5-7.5'
+        />
+      </svg>
+    );
+  };
   const data = [
     {
       title: '경매는 어떤 식으로 진행하나요 ? (1)',
