5f9ae974b8a19517a0b0d735ae77d8e436f2a878
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-21 10:55:02 +0900

    feat: 객실 등록 첫 단계 Room1 컴포넌트 생성

---
+++ src/pages/RoomRegistration/Room1.jsx
@@ -0,0 +1,22 @@
+import roomIcon from '../../assets/icons/room_icon.png';
+import './Ste1.css';
+
+const Room1 = () => {
+  return (
+    <div className='room1-container'>
+      <img src={roomIcon} alt='' />
+
+      <h1>당신의 객실을 등록하세요!</h1>
+
+      <div className='room1-content flex flex-col items-center justify-center'>
+        <h3>객실 등록 순서</h3>
+        <ul>
+          <li>🚩 객실 정보 입력</li>
+          <li>🚩 객실 사진 제출</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Room1;
