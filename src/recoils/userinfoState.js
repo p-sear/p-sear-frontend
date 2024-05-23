fa3fe2a9860945e012da4955146454eded483f91
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-23 14:46:37 +0900

    refactor: userinfo recoil 상태 업데이트

--- src/recoils/userinfoState.js
+++ src/recoils/userinfoState.js
@@ -2,7 +2,13 @@ import { atom } from 'recoil';
 
 const userinfoState = atom({
   key: 'userinfoState',
-  default: { id: undefined, email: undefined, username: undefined },
+  default: {
+    id: undefined,
+    email: undefined,
+    username: undefined,
+    tel: undefined,
+    description: undefined,
+  },
 });
 
 export default userinfoState;
