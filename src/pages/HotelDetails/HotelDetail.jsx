740dabbc89cc0b3d2794a960eb4da9aae7d75dd5
LEE <dlcksgur0919@naver.com>
2024-05-23 16:04:55 +0900

    feat : 숙소상세페이지 api 연동

--- src/pages/HotelDetails/HotelDetail.jsx
+++ src/pages/HotelDetails/HotelDetail.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
+
 import { Button, Card, Typography } from '@material-tailwind/react';
+import axios from 'axios';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
@@ -13,64 +16,52 @@ import HotelImage from './hotelImage';
 import RoomCard from './roomcard';
 
 const HotelDetail = () => {
+  const [roomData, setRoomData] = useState([]);
+  const [recommendedHotels, setRecommendedHotels] = useState([]);
+  const [hotelDetails, setHotelDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchRoomData = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:5173/dummy/roomList.json',
+        );
+        setRoomData(response.data.body);
+      } catch (error) {
+        console.error('객실 리스트 오류', error);
+      }
+    };
+
+    const fetchRecommendedHotels = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:5173/dummy/recommendedList.json',
+        );
+        setRecommendedHotels(response.data.body);
+      } catch (error) {
+        console.error('추천 숙소 오류', error);
+      }
+    };
+
+    const fetchHotelDetails = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:5173/dummy/hotel.json',
+        );
+        setHotelDetails(response.data.body);
+      } catch (error) {
+        console.error('호텔 설명 오류', error);
+      }
+    };
+
+    fetchHotelDetails();
+    fetchRoomData();
+    fetchRecommendedHotels();
+  }, []);
+
   const handleButtonClick = () => {
     router.navigate('/hotel-reservation');
   };
-
-  const roomData = [
-    {
-      id: '디럭스 트윈',
-      name: '디럭스 트윈',
-      description: '침대 두개.',
-      imageUrl:
-        'https://res.klook.com/image/upload/c_fill,w_627,h_470/q_80/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/awessankdbaeuib4xjks.webp',
-    },
-    {
-      id: '디럭스 트윈',
-      name: '디럭스 트윈',
-      description: '침대 두개.',
-      imageUrl:
-        'https://res.klook.com/image/upload/c_fill,w_627,h_470/q_80/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/awessankdbaeuib4xjks.webp',
-    },
-    {
-      id: '디럭스 트윈',
-      name: '디럭스 트윈',
-      description: '침대 두개.',
-      imageUrl:
-        'https://res.klook.com/image/upload/c_fill,w_627,h_470/q_80/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/awessankdbaeuib4xjks.webp',
-    },
-  ];
-
-  const recommendedHotels = [
-    {
-      id: '블랙.5성급.호텔',
-      name: '서울신라호텔',
-      description: '가격',
-      imageUrl:
-        'https://i.namu.wiki/i/_VdL80a6q8YfJ3ob0cH0g6M4C4u3eafyHQV8oHFnZetT7yEjHPC8hybEh7-Xwfz6H6S4EkwBn6mkLvhb7rGscQ.webp',
-    },
-    {
-      id: '블랙.5성급.호텔',
-      name: '강원도 신라호텔2',
-      description: '가격',
-      imageUrl:
-        'https://i.namu.wiki/i/_VdL80a6q8YfJ3ob0cH0g6M4C4u3eafyHQV8oHFnZetT7yEjHPC8hybEh7-Xwfz6H6S4EkwBn6mkLvhb7rGscQ.webp',
-    },
-    {
-      id: '블랙.5성급.호텔',
-      name: '인천신라호텔3',
-      description: '가격',
-      imageUrl:
-        'https://i.namu.wiki/i/_VdL80a6q8YfJ3ob0cH0g6M4C4u3eafyHQV8oHFnZetT7yEjHPC8hybEh7-Xwfz6H6S4EkwBn6mkLvhb7rGscQ.webp',
-    },
-    {
-      id: '블랙.5성급.호텔',
-      name: '시흥 신라호텔4',
-      description: '가격 ',
-      imageUrl:
-        'https://i.namu.wiki/i/_VdL80a6q8YfJ3ob0cH0g6M4C4u3eafyHQV8oHFnZetT7yEjHPC8hybEh7-Xwfz6H6S4EkwBn6mkLvhb7rGscQ.webp',
-    },
-  ];
 
   const sliderSettings = {
     dots: false,
@@ -113,63 +104,131 @@ const HotelDetail = () => {
         <HotelImage />
       </div>
 
-      <div className='m-4 flex flex-col items-start justify-between rounded-lg bg-gray-50 p-6 shadow-md lg:flex-row lg:items-center'>
-        <div className='w-full lg:mr-4 lg:w-3/4'>
-          <Card className='bg-white p-6 shadow-md'>
-            <Typography variant='h6' color='black' className='mb-2'>
-              5성급 호텔
-            </Typography>
-            <Typography variant='h3' color='black' className='mb-4'>
-              히든 클리프 호텔&네이쳐
-            </Typography>
-            <Typography variant='h5' color='black' className='mb-4'>
-              서비스 및 부대시설
-            </Typography>
-            <div className='mb-4 flex flex-wrap'>
-              <span className='m-1 rounded bg-gray-200 px-3 py-1'>
-                스파/월풀
-              </span>
-              <span className='m-1 rounded bg-gray-200 px-3 py-1'>노래방</span>
-              <span className='m-1 rounded bg-gray-200 px-3 py-1'>파티룸</span>
-              <span className='m-1 rounded bg-gray-200 px-3 py-1'>
-                사우나/찜질방
-              </span>
-              <span className='m-1 rounded bg-gray-200 px-3 py-1'>커플 PC</span>
-              <span className='m-1 rounded bg-gray-200 px-3 py-1'>
-                트윈베드
-              </span>
+      <div
+        key={hotelDetails.id}
+        className='m-4 flex flex-col items-start justify-between rounded-lg bg-gray-50 p-6 shadow-md lg:flex-row lg:items-center'
+      >
+        {hotelDetails.map(hotelDetail => (
+          <>
+            <div className=' group-disabled:w-full lg:mr-4 lg:w-3/4'>
+              <Card className='bg-white p-6 shadow-md'>
+                <Typography variant='h6' color='black' className='mb-2'>
+                  {hotelDetail.category}
+                </Typography>
+                <Typography variant='h3' color='black' className='mb-4'>
+                  {hotelDetail.name}
+                </Typography>
+                <Typography variant='h5' color='black' className='mb-4'>
+                  서비스 및 부대시설
+                </Typography>
+                <div className='mb-4 flex flex-wrap'>
+                  {hotelDetail.wifi && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      WIFI
+                    </span>
+                  )}
+                  {hotelDetail.barbecue && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      바베큐
+                    </span>
+                  )}
+                  {hotelDetail.sauna && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      사우나/스파
+                    </span>
+                  )}
+                  {hotelDetail.swimmingPool && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      수영장
+                    </span>
+                  )}
+                  {hotelDetail.restaurant && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      레스토랑
+                    </span>
+                  )}
+                  {hotelDetail.roofTop && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      루프탑
+                    </span>
+                  )}
+                  {hotelDetail.fitness && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      피트니스
+                    </span>
+                  )}
+                  {hotelDetail.dryer && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      건조기
+                    </span>
+                  )}
+                  {hotelDetail.breakfast && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      조식
+                    </span>
+                  )}
+                  {hotelDetail.smokingArea && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      흡연구역
+                    </span>
+                  )}
+                  {hotelDetail.allTimeDesk && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      24시간 데스크
+                    </span>
+                  )}
+                  {hotelDetail.luggageStorage && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      짐보관
+                    </span>
+                  )}
+                  {hotelDetail.snackBar && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      스낵바
+                    </span>
+                  )}
+                  {hotelDetail.petFriendly && (
+                    <span className='m-1 rounded bg-gray-200 px-3 py-1'>
+                      반려동물동반
+                    </span>
+                  )}
+                </div>
+                <Typography variant='h4' color='black' className='mb-2'>
+                  숙소 소개
+                </Typography>
+                <Typography variant='body1' color='black'>
+                  &quot;{hotelDetail.description}&quot;
+                </Typography>
+              </Card>
             </div>
-            <Typography variant='h4' color='black' className='mb-2'>
-              숙소 소개
-            </Typography>
-            <Typography variant='body1' color='black'>
-              &quot;전통이라는 지붕 위에 모더니즘적 디자인 요소를 가미, 삶에
-              여유와 품격을 한층 높여 주는 프리미엄 라이프스타일 공간으로 변화를
-              거듭해 오는 세계 최고의 럭셔리 호텔입니다&quot;
-            </Typography>
-          </Card>
-        </div>
+            <div className='flex w-full flex-col space-y-4 lg:w-1/4'>
+              <div className='h-40 w-72 rounded-lg'>
+                <Map
+                  center={{
+                    lat: hotelDetail.latitude,
+                    lng: hotelDetail.longtitude,
+                  }}
+                  style={{ width: '100%', height: '100%' }}
+                  level={3}
+                  className='rounded-lg'
+                >
+                  <MapMarker
+                    position={{
+                      lat: hotelDetail.latitude,
+                      lng: hotelDetail.longtitude,
+                    }}
+                  ></MapMarker>
+                </Map>
+              </div>
+              <DateSelector />
+              <PeopleSelector />
 
-        <div className='flex w-full flex-col space-y-4 lg:w-1/4'>
-          <div className='h-40 w-72 rounded-lg'>
-            <Map
-              center={{ lat: 37.55605010732486, lng: 127.00496835047365 }}
-              style={{ width: '100%', height: '100%' }}
-              level={3}
-              className='rounded-lg'
-            >
-              <MapMarker
-                position={{ lat: 37.55605010732486, lng: 127.00496835047365 }}
-              ></MapMarker>
-            </Map>
-          </div>
-          <DateSelector />
-          <PeopleSelector />
-
-          <Button color='blue' fullWidth onClick={handleButtonClick}>
-            예약하기
-          </Button>
-        </div>
+              <Button color='blue' fullWidth onClick={handleButtonClick}>
+                예약하기
+              </Button>
+            </div>
+          </>
+        ))}
       </div>
 
       <div className='m-4'>
@@ -180,7 +239,6 @@ const HotelDetail = () => {
           </div>
         </Card>
       </div>
-
       <div className='mx-auto mb-8 w-full lg:w-2/3'>
         {roomData.map(room => (
           <RoomCard
@@ -192,7 +250,6 @@ const HotelDetail = () => {
           />
         ))}
       </div>
-
       <div className=''>
         <Typography variant='h4' color='black' className='mb-4'>
           추천 숙소
@@ -201,11 +258,7 @@ const HotelDetail = () => {
           {recommendedHotels.map(hotel => (
             <div key={hotel.id} className='p-3'>
               <div className=''>
-                <RecommendCard
-                  name={hotel.name}
-                  description={hotel.description}
-                  imageUrl={hotel.imageUrl}
-                />
+                <RecommendCard name={hotel.name} imageUrl={hotel.imageUrl} />
               </div>
             </div>
           ))}
