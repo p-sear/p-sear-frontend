1953a591a46630aba2a5dc267d552ec76ddd7519
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-26 10:00:20 +0900

    refactor: 숙소 예약 내역 조회 API 호출

--- src/pages/MyPage/MyReservation.jsx
+++ src/pages/MyPage/MyReservation.jsx
@@ -1,36 +1,81 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 import hotelImg from '../../assets/images/hotel.png';
 import './MyReservation.css';
 
 const MyReservation = () => {
-  const reservations = [
-    {
-      id: 1,
-      photo: hotelImg,
-      name: '호텔 A',
-      roomType: '스탠다드',
-      checkIn: '2024.05.20',
-      checkOut: '2024.05.22',
-      price: '100,000',
-    },
-    {
-      id: 2,
-      photo: hotelImg,
-      name: '호텔 B',
-      roomType: '스위트',
-      checkIn: '2024.05.20',
-      checkOut: '2024.05.22',
-      price: '100,000',
-    },
-    {
-      id: 3,
-      photo: hotelImg,
-      name: '호텔 C',
-      roomType: '스탠다드',
-      checkIn: '2024.05.20',
-      checkOut: '2024.05.22',
-      price: '100,000',
-    },
-  ];
+  const [reservations, setReservations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchReservations = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:5173/dummy/myReservation.json',
+        );
+
+        const myReservationDatas = response.data.body.content.map(
+          myReservation => ({
+            id: myReservation.id,
+            name: myReservation.name,
+            roomType: myReservation.roomType,
+            checkIn: myReservation.checkIn,
+            checkOut: myReservation.checkOut,
+            price: myReservation.price,
+            photo: myReservation.mainImage || hotelImg,
+          }),
+        );
+        setReservations(myReservationDatas);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchReservations();
+  }, []);
+
+  if (loading) {
+    return <div>로딩 중...</div>;
+  }
+
+  if (error) {
+    return <div>에러 발생: {error.message}</div>;
+  }
+
+  // const reservations = [
+  //   {
+  //     id: 1,
+  //     photo: hotelImg,
+  //     name: '호텔 A',
+  //     roomType: '스탠다드',
+  //     checkIn: '2024.05.20',
+  //     checkOut: '2024.05.22',
+  //     price: '100,000',
+  //   },
+  //   {
+  //     id: 2,
+  //     photo: hotelImg,
+  //     name: '호텔 B',
+  //     roomType: '스위트',
+  //     checkIn: '2024.05.20',
+  //     checkOut: '2024.05.22',
+  //     price: '100,000',
+  //   },
+  //   {
+  //     id: 3,
+  //     photo: hotelImg,
+  //     name: '호텔 C',
+  //     roomType: '스탠다드',
+  //     checkIn: '2024.05.20',
+  //     checkOut: '2024.05.22',
+  //     price: '100,000',
+  //   },
+  // ];
 
   return (
     <div className='myreservation-container flex flex-col'>
@@ -43,7 +88,7 @@ const MyReservation = () => {
             className='myreservation-item flex w-full items-center justify-between gap-20'
           >
             <div className='flex h-full items-center'>
-              <img src={hotelImg} alt='' />
+              <img src={reservation.photo || hotelImg} alt='' />
 
               <div className='flex h-full items-center'>
                 <div className='flex h-full flex-col justify-between'>
