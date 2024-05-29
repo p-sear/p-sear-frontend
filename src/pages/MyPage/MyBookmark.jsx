ae67f5bd5a75ea414045e96a6635c3b19e76c900
ChoiHyebin <bini0819@catholic.ac.kr>
2024-05-29 16:55:54 +0900

    feat: 찜 목록 조회 API 호출, 찜한 숙소 삭제 API 호출

--- src/pages/MyPage/MyBookmark.jsx
+++ src/pages/MyPage/MyBookmark.jsx
@@ -1,46 +1,81 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
 import { GoHeartFill } from 'react-icons/go';
 
-import hotelImg from '../../assets/images/hotel.png';
+import pserLoading from '../../assets/images/loading.png';
 import './MyBookmark.css';
 
 const MyBookmark = () => {
-  const reservations = [
-    {
-      id: 1,
-      photo: hotelImg,
-      name: '호텔 A',
-      price: '150,000',
-    },
-    {
-      id: 2,
-      photo: hotelImg,
-      name: '호텔 B',
-      price: '90,000',
-    },
-    {
-      id: 3,
-      photo: hotelImg,
-      name: '호텔 C',
-      price: '88,000',
-    },
-  ];
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5173/dummy/bookmark.json')
+      .then(response => {
+        setReservations(response.data.bookmark);
+      })
+      .catch(error => {
+        console.error('찜 목록 조회 API 호출 에러:', error);
+      });
+  }, []);
+
+  // 숙소 찜(즐겨찾기) 취소 함수
+  const cancelBookmark = hotel_id => {
+    axios
+      .delete('http://localhost:5173/dummy/bookmark.json', {
+        data: { status: 0, hotel_id },
+      })
+      .then(response => {
+        console.log(response.data.message);
+        // 즐겨찾기 취소 성공 후 목록 갱신
+        setReservations(
+          reservations.filter(item => item.hotel_id !== hotel_id.toString()),
+        );
+      })
+      .catch(error => {
+        console.error('찜한 숙소 삭제 API 호출 에러:', error);
+      });
+  };
+
+  // const reservations = [
+  //   {
+  //     id: 1,
+  //     photo: hotelImg,
+  //     name: '호텔 A',
+  //     price: '150,000',
+  //   },
+  //   {
+  //     id: 2,
+  //     photo: hotelImg,
+  //     name: '호텔 B',
+  //     price: '90,000',
+  //   },
+  //   {
+  //     id: 3,
+  //     photo: hotelImg,
+  //     name: '호텔 C',
+  //     price: '88,000',
+  //   },
+  // ];
 
   return (
     <div className='mybookmark-container flex flex-col'>
       <h1>찜한 숙소</h1>
 
       <div className='mybookmark-box flex w-full flex-col items-center justify-center'>
-        {reservations.map(reservation => (
+        {reservations.map((reservation, index) => (
           <div
-            key={reservations.id}
+            key={index}
             className='mybookmark-item flex w-full items-center justify-between gap-20'
           >
             <div className='flex h-full items-center'>
               <div className='mybookmark-img relative'>
-                <img src={reservation.photo} alt='' />
+                <img src={reservation.hotelImage || pserLoading} alt='' />
                 <GoHeartFill
                   className='mybookmark-heart absolute'
                   size={'30px'}
+                  onClick={() => cancelBookmark(reservation.hotel_id)}
                 />
               </div>
 
@@ -50,7 +85,7 @@ const MyBookmark = () => {
                   <p>숙박 가격</p>
                 </div>
                 <div className='flex h-full flex-col justify-evenly font-bold'>
-                  <p>{reservation.name}</p>
+                  <p>{reservation.hotelName}</p>
                   <p>{reservation.price} 원</p>
                 </div>
               </div>
