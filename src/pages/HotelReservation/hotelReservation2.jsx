348ec573676c178cd030517cdc538680b0c91e83
LEE <dlcksgur0919@naver.com>
2024-05-30 10:07:32 +0900

    feat : 결제전 예약페이지 유효성 추가 및 리팩토링

--- src/pages/HotelReservation/hotelReservation2.jsx
+++ src/pages/HotelReservation/hotelReservation2.jsx
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+
 import { Input, Typography } from '@material-tailwind/react';
-import { Button, Checkbox } from '@material-tailwind/react';
+import { Button, Radio } from '@material-tailwind/react';
+import { useLocation } from 'react-router-dom';
 
 const HotelReservation2 = () => {
+  const location = useLocation();
+  const hotelName = location.state?.hotelName;
+  const selectedRoom = location.state?.selectedRoom;
+
+  const [name, setName] = useState('');
+  const [phoneNum, setPhoneNum] = useState('');
+  const [visitMethod, setVisitMethod] = useState('');
+
+  console.log(hotelName);
+  console.log(selectedRoom);
+  console.log(name);
+  console.log(phoneNum);
+
+  const handlePhoneNumChange = e => {
+    const input = e.target.value;
+    const isNumber = /^[0-9]*$/;
+    if (isNumber.test(input) || input === '') {
+      setPhoneNum(input);
+    }
+  };
+
+  const handleVisitMethodChange = method => {
+    setVisitMethod(method);
+  };
+
   return (
-    <div className='flex  flex-col items-center justify-center'>
+    <div className='flex flex-col items-center justify-center'>
       <div>
         <Typography
           variant='h1'
@@ -20,7 +48,9 @@ const HotelReservation2 = () => {
           color='blue'
           label='성명'
           placeholder='성명'
-          className=' mx-auto'
+          className='mx-auto'
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
@@ -28,27 +58,39 @@ const HotelReservation2 = () => {
           color='blue'
           label='연락처'
           placeholder='연락처'
-          className=' mx-auto'
+          className='mx-auto'
+          value={phoneNum}
+          onChange={handlePhoneNumChange}
         />
       </div>
       <Typography variant='h6' color='blue' className='mx-auto mt-4'>
         방문방법
       </Typography>
       <div className='flex w-max gap-4'>
-        <Checkbox color='blue' label='도보 방문' />
-        <Checkbox color='blue' label='차량 방문' />
+        <Radio
+          color='blue'
+          label='도보 방문'
+          checked={visitMethod === '도보 방문'}
+          onChange={() => handleVisitMethodChange('도보 방문')}
+        />
+        <Radio
+          color='blue'
+          label='차량 방문'
+          checked={visitMethod === '차량 방문'}
+          onChange={() => handleVisitMethodChange('차량 방문')}
+        />
       </div>
 
       <Typography variant='h6' color='blue' className='mx-auto mt-4'>
         약관 동의
       </Typography>
       <div className='flex w-max flex-col gap-1'>
-        <Checkbox
+        <Radio
           color='blue'
           label='숙소 이용규칙 및 취소/환불규정 동의 (필수)'
         />
-        <Checkbox color='blue' label='개인정보 수집 및 이용 동의 (필수)' />
-        <Checkbox color='blue' label='개인정보 제 3자 제공 동의 (필수)' />
+        <Radio color='blue' label='개인정보 수집 및 이용 동의 (필수)' />
+        <Radio color='blue' label='개인정보 제 3자 제공 동의 (필수)' />
       </div>
 
       <Typography variant='h6' color='blue' className='mx-auto mt-4'>
