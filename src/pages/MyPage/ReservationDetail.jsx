4dd984ca8d4d5c7dd129fca4475f54f8ec02f62d
ChoiHyebin <bini0819@catholic.ac.kr>
2024-06-03 11:22:59 +0900

    feat: 숙소 예약 상세 조회 API 연동

--- src/pages/MyPage/ReservationDetail.jsx
+++ src/pages/MyPage/ReservationDetail.jsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
 import { FaArrowLeftLong, FaStar } from 'react-icons/fa6';
 
 import pserLoading from '../../assets/images/loading.png';
 import './ReservationDetail.css';
 
 const ReservationDetail = () => {
+  const [reservationData, setReservationData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5173/dummy/myReservation.json`,
+        );
+        setReservationData(response.data.body.content[0]); // 첫 번째 예약 데이터 사용
+        setLoading(false);
+      } catch (error) {
+        console.error('숙소 예약 조회 API 호출 실패:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>정보 불러오는 중...</p>;
+  }
+
+  if (!reservationData) {
+    return <p>예약 내역을 조회할 수 없습니다.</p>;
+  }
+
+  const services = [
+    { name: '주차', available: reservationData.parkingLot },
+    { name: '와이파이', available: reservationData.wifi },
+    { name: 'BBQ', available: reservationData.barbecue },
+    { name: '사우나', available: reservationData.sauna },
+    { name: '수영장', available: reservationData.swimmingPool },
+    { name: '레스토랑', available: reservationData.restaurant },
+    { name: '루프탑', available: reservationData.roofTop },
+    { name: '피트니스', available: reservationData.fitness },
+    { name: '건조기', available: reservationData.dryer },
+    { name: '조식', available: reservationData.breakfast },
+    { name: '흡연 구역', available: reservationData.smokingArea },
+    { name: '24시간 데스크', available: reservationData.allTimeDesk },
+    { name: '짐 보관', available: reservationData.luggageStorage },
+    { name: '매점', available: reservationData.snackBar },
+    { name: '반려견 동반', available: reservationData.petFriendly },
+  ].filter(service => service.available);
+
   return (
     <div className='resDetail flex items-center justify-center'>
       <div className='resDetail-container flex flex-col justify-center gap-5'>
@@ -16,28 +65,31 @@ const ReservationDetail = () => {
 
         <div className='resDetail-list flex w-full flex-col justify-center gap-12'>
           <div className='hotel-card w-full'>
-            <img src={pserLoading} className='w-full' />
+            <img
+              src={reservationData.mainImage || pserLoading}
+              className='w-full'
+            />
 
             <div>
               <div className='p-5'>
-                <p>숙소 위치</p>
-                <h3 className='text-blue-600'>숙소 이름</h3>
+                <p>{reservationData.city}</p>
+                <h3 className='text-blue-600'>{reservationData.name}</h3>
                 <p className='res-grade relative bg-white'>
                   <FaStar className='star-icon absolute' />
-                  평점
+                  {reservationData.rating}
                 </p>
               </div>
               <hr style={{ border: '1px solid #E7E7E7' }} />
               <div className='flex items-center justify-between p-5'>
                 <div>
                   <p className='text-xs'>체크인</p>
-                  <b>2024년 6월 13일</b>
+                  <b>{reservationData.checkIn}</b>
                   <p>15:00</p>
                 </div>
                 <p className='out-in'>4박</p>
                 <div>
                   <p className='text-xs'>체크아웃</p>
-                  <b>2024년 6월 17일</b>
+                  <b>{reservationData.checkOut}</b>
                   <p>11:00</p>
                 </div>
               </div>
@@ -48,10 +100,16 @@ const ReservationDetail = () => {
             <div className='flex flex-col gap-3 p-5'>
               <h3>객실 정보</h3>
               <div className='flex gap-3'>
-                <img src={pserLoading} className='res-room-img' />
+                <img
+                  src={reservationData.mainImage || pserLoading}
+                  className='res-room-img'
+                />
+                {/* 객실 이미지 사진으로 교체해야 함. */}
                 <div className='flex flex-col gap-2'>
-                  <b>객실 이름</b>
-                  <p>객실 너비 | 금연 | 침대 사이즈</p>
+                  <b>{reservationData.roomType}</b>
+                  <p>
+                    {reservationData.category} | 객실 너비 | 금연 | 침대 사이즈
+                  </p>
                 </div>
               </div>
             </div>
@@ -61,8 +119,11 @@ const ReservationDetail = () => {
             <div className='flex flex-col gap-2 p-5'>
               <p>객실 서비스</p>
               <div className='flex flex-wrap items-center gap-3'>
-                <span className='res-services'>Wifi</span>
-                <span className='res-services'>금연</span>
+                {services.map(service => (
+                  <span key={service.name} className='res-services'>
+                    {service.name}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
