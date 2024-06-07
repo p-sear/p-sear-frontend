9f975100153530655bce440bd349394124f30ba6
ChoiHyebin <bini0819@catholic.ac.kr>
2024-06-08 08:48:07 +0900

    💄

--- src/helpers/Spinner.jsx
+++ src/helpers/Spinner.jsx
@@ -1,9 +1,18 @@
-import SpinnerImg from '../assets/images/Spinner.gif';
+import SpinnerImg from '../assets/images/spinner.gif';
 
 const Spinner = () => {
+  const spinnerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  };
+
   return (
-    <div className='spinner-containe'>
-      <img src={SpinnerImg} alt='' />
+    <div className='spinner-containe' style={spinnerStyle}>
+      <img src={SpinnerImg} style={{ width: '100px' }} />
+      <p>로딩 중입니다. 잠시만 기다려 주세요.</p>
     </div>
   );
 };
