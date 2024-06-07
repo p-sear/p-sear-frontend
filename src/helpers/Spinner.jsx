acf25a97480e77b62130bd169d6ea8107512054c
ChoiHyebin <bini0819@catholic.ac.kr>
2024-06-08 08:25:35 +0900

    feat: 로딩 시간 동안 보여줄 스피너 생성

---
+++ src/helpers/Spinner.jsx
@@ -0,0 +1,11 @@
+import SpinnerImg from '../assets/images/Spinner.gif';
+
+const Spinner = () => {
+  return (
+    <div className='spinner-containe'>
+      <img src={SpinnerImg} alt='' />
+    </div>
+  );
+};
+
+export default Spinner;
