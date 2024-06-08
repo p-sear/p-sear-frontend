684b0c50389ebba5f36e207040759b88be8306c8
wooyong99 <wulovesk@naver.com>
2024-06-08 20:32:04 +0900

    refactor : userinfoState 불필요 데이터 삭제

--- src/recoils/userinfoState.js
+++ src/recoils/userinfoState.js
@@ -3,12 +3,8 @@ import { atom } from 'recoil';
 const userinfoState = atom({
   key: 'userinfoState',
   default: {
-    id: undefined,
     email: undefined,
-    username: undefined,
     password: undefined,
-    tel: undefined,
-    description: undefined,
   },
 });
 
