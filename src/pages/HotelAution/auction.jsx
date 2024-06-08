92619ccda97ff94365a496301657f535c2d6016d
LEE <dlcksgur0919@naver.com>
2024-06-08 13:10:16 +0900

    feat : 경매 입찰 api 수정

--- src/pages/HotelAution/auction.jsx
+++ src/pages/HotelAution/auction.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   Accordion,
@@ -13,14 +11,14 @@ import { Button } from '@material-tailwind/react';
 import axios from 'axios';
 
 const Auction = () => {
-  const [open, setOpen] = React.useState(1);
+  const [open, setOpen] = useState(1);
   const handleOpen = value => setOpen(open === value ? 0 : value);
   const [auctionData, setAuctionData] = useState([]);
   const [latestAuction, setLatestAuction] = useState({ date: '', price: '' });
   const [inputPrice, setInputPrice] = useState('');
 
   useEffect(() => {
-    const fetchAuctionData = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get(
           'http://localhost:5173/dummy/auctionData.json',
@@ -31,12 +29,24 @@ const Auction = () => {
           setLatestAuction({ date: latest.date, price: latest.price });
         }
       } catch (error) {
-        console.error('경매 데이터 오류', error);
+        console.error('Error fetching auction data:', error);
       }
     };
 
-    fetchAuctionData();
-  });
+    fetchData();
+  }, []);
+
+  const postBidData = async () => {
+    try {
+      const url = 'http://1.228.166.90:8000/auctions/{auctionId}/deposits';
+      const data = { price: inputPrice };
+      const response = await axios.post(url, data);
+      console.log('Server response:', response.data);
+    } catch (error) {
+      console.error('Error posting bid data:', error);
+    }
+  };
+
   const handleInputPriceChange = e => {
     const input = e.target.value;
     const isNumber = /^[0-9]*$/;
@@ -45,51 +55,51 @@ const Auction = () => {
     }
   };
 
-  console.log(inputPrice);
   return (
-    <div>
-      <div className='flex justify-center'>
-        <Accordion
-          open={open === 0}
-          className=' m-4 w-2/3 rounded-lg bg-gray-50 px-4 shadow-md'
-        >
-          <AccordionHeader className='flex justify-between'>
-            <Typography variant='h5' className='ml-8 mr-8'>
-              남은시간 : 72 : 00 : 00
+    <div className='flex justify-center'>
+      <Accordion
+        open={open === 0}
+        className=' m-4 w-2/3 rounded-lg bg-gray-50 px-4 shadow-md'
+      >
+        <AccordionHeader className='flex justify-between'>
+          <Typography variant='h5' className='ml-8 mr-8'>
+            남은시간 : 72 : 00 : 00
+          </Typography>
+
+          <Typography variant='h5' className='mr-4'>
+            {latestAuction.date} {latestAuction.price}
+          </Typography>
+
+          <div className='mr-8 w-2/12'>
+            <Input
+              label='금액 입력'
+              value={inputPrice}
+              onChange={handleInputPriceChange}
+            />
+          </div>
+
+          <Button color='blue' onClick={postBidData}>
+            입찰
+          </Button>
+
+          <div className='justify-end'>
+            <Button color='blue' onClick={() => handleOpen(1)}>
+              더보기
+            </Button>
+          </div>
+        </AccordionHeader>
+
+        {auctionData.map(data => (
+          <AccordionBody className='' key={data.price}>
+            <Typography variant='h5' className='float-left'>
+              {data.date}
             </Typography>
-
-            <Typography variant='h5' className='mr-4'>
-              {latestAuction.date} {latestAuction.price}
+            <Typography variant='h5' className='text-right'>
+              {data.price}
             </Typography>
-
-            <div className='mr-8 w-2/12'>
-              <Input
-                label='금액 입력'
-                value={inputPrice}
-                onChange={handleInputPriceChange}
-              ></Input>
-            </div>
-
-            <Button color='blue'>입찰</Button>
-            <div className='justify-end'>
-              <Button color='blue' onClick={() => handleOpen(1)}>
-                더보기
-              </Button>
-            </div>
-          </AccordionHeader>
-
-          {auctionData.map(data => (
-            <AccordionBody className='' key={data.price}>
-              <Typography variant='h5' className='float-left'>
-                {data.date}
-              </Typography>
-              <Typography variant='h5' className='text-right'>
-                {data.price}
-              </Typography>
-            </AccordionBody>
-          ))}
-        </Accordion>
-      </div>
+          </AccordionBody>
+        ))}
+      </Accordion>
     </div>
   );
 };
